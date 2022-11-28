c70f9dedf78fe64b0de8d7f6eb2e90668bc46bdf
jrys3i <90259372+jrys3i@users.noreply.github.com>
2022-11-28 09:06:55 +0100

    Add files via upload

---
+++ sprzawdzian 28.11.2022/sklep.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl2.css">
+    <title>Warzywniak</title>
+</head>
+<body>
+    <header>
+<section class="blok-lewy">
+       <h1>Internetowy sklep z eko warzywami</h1>     
+</section>
+<section class="blok-prawy">
+<ol class="lista">
+    <li>Warzywa</li>
+    <li>Owoce</li>
+    <li><a href="https://terapiasokami.pl/">Soki</a></li>
+</ol>
+</section>
+</header>
+<section class="blok-glowny">
+<div class="item">
+            <img src="gruszka.jpg" alt="gruszka">
+            <h5>Gruszka</h5>
+            <p>Opis: Klaps</p>
+            <p>Na stanie: 20</p>
+            <p>2.56zł</p>
+        </div>
+        <div class="item">
+            <img src="morela.jpg" alt="morela">
+            <h5>morela</h5>
+            <p>Opis</p>
+            <p>Na stanie: 30</p>
+            <p>5.78zł</p>
+        </div>
+        <div class="item">
+            <img src="arbuz.jpg" alt="arbuz">
+            <h5>Arbuz</h5>
+            <p>Opis: Czerwony</p>
+            <p>Na stanie: 2</p>
+            <p>2.56zł</p>
+        </div>
+        <div class="item">
+            <img src="papaja.jpg" alt="papaja">
+            <h5>Papaja</h5>
+            <p>Opis: papaja</p>
+            <p>Na stanie: 7</p>
+            <p>4.60zł</p>
+        </div>
+        <div class="item">
+            <img src="papryka.jpg" alt="papryka">
+            <h5>Papryka</h5>
+            <p>Opis: papryka</p>
+            <p>Na stanie: 12</p>
+            <p>2.50zł</p>
+        </div>
+        <div class="item">
+            <img src="ziemniak.jpg" alt="ziemniak">
+            <h5>Ziemniak</h5>
+            <p>Opis: Ziemniak</p>
+            <p>Na stanie: 43</p>
+            <p>5zł</p>
+        </div>
+</section>
+<footer>
+    <form action="" method="post">
+    <input type="text" placeholder="Nazwa">
+    <input type="text" placeholder="Cena">
+    <input type="button" value="Dodaj produkt">
+    </form>
+</footer>            
+</body>
+</html>
