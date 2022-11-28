6869d7d70a18b7e40c6a596d85bdcdf66fcde837
jrys3i <90259372+jrys3i@users.noreply.github.com>
2022-11-28 08:44:37 +0100

    Add files via upload

---
+++ sprzawdzian 28.11.2022/sklep.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl2.css">
+    <title>Warzywniak</title>
+</head>
+<body>
+<section class="blok-lewy">
+       <h1>Internetowy sklep z eko warzywami</h1>     
+</section>
+<section class="blok-prawy">
+<ol class="lista">
+    <li>Warzywa</li>
+    <li>Owoce</li>
+    <li><a href="https://terapiasokami.pl/">Soki</a></li>
+</ol>
+</section>
+<section class="blok-glowny">
+    <div>
+        <img src="gruszka.jpg">
+        <h2>Gruszka</h2>
+    </div>
+    <div>
+        <img src="morela.jpg" alt="">
+    <h2>Morela</h2>
+    </div>
+    <div>
+        <img src="arbuz.jpg" alt="">
+    <h2>Arbuz</h2>
+    </div>
+    <div>
+        <img src="papaja.jpg" alt="">
+    <h2>Papaja</h2>
+    </div>
+    <div>
+        <img src="marchew.jpg" alt="">
+    <h2>Marchew</h2>
+    </div>
+    <div>
+        <img src="ziemniak.jpg" alt="">
+    <h2>Ziemniak</h2>
+    </div>
+</section>
+<footer>
+    <form action="" method="post">
+    <input type="text" placeholder="Nazwa">
+    <input type="text" placeholder="Cena">
+    <input type="button" value="Dodaj produkt">
+    </form>
+</footer>            
+</body>
+</html>
